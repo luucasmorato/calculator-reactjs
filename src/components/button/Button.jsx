cefb16ddde8c7c21a13532ca3ccf385a7ce0cd05
Lucas Morato <luucasmorato@gmail.com>
2020-07-25 15:38:51 -0300

    add prettier

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Button.css";
 
-export default props => (
+export default (props) => (
   <button
     className={`
     button
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Calculator from './main/Calculator';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculator from "./main/Calculator";
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
-
+ReactDOM.render(<Calculator />, document.getElementById("root"));
